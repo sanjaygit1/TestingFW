218c09eec0a57af2fbaa9bb300da79b9690ffecf
sanjaygit1 <rathinreddy14@gmail.com>
2024-10-29 07:26:40 +0530

    push1

--- Selenium7/src/test/java/demo/Sampletest.java
+++ Selenium7/src/test/java/demo/Sampletest.java
@@ -3,15 +3,8 @@ package demo;
 public class Sampletest {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello");
 
-	System.out.println("Gud mrng");
-	
-	System.out.println("Welcome");
-
-		System.out.println("Bye");
-			
+		System.out.println("Method running");
 	}
 
 }
